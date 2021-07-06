ee83619088142d36ce7346074123c361a4c6c098
Morgan Stockham <morganstockham@gmail.com>
2021-07-06 16:21:25 -0400

    traveltime code up

--- src/06_park_polygons.R
+++ src/06_park_polygons.R
@@ -11,48 +11,84 @@ library(purrr)
 library(osrm)
 library(rmapzen)
 library(rgdal)
+library(rgeos)
 
+parks <- st_read("./data/original/arlington_parks/Park_Polygons.shp")
+parks = parks %>%
+  st_as_sf(coords = c("long","lat")) %>%
+  st_transform("+proj=longlat +datum=WGS84")
+
+colors <- c("#232d4b","#2c4f6b","#0e879c","#60999a","#d1e0bf","#d9e12b","#e6ce3a","#e6a01d","#e57200","#fdfdfd")
 
 #
-# Polygons + Intersections ---------------------------------------
+# Centroids ---------------------------------------
 #
 
+centroid <- st_centroid(parks)
+centroid = centroid %>%
+  st_as_sf(coords = c("long","lat")) %>%
+  st_transform("+proj=longlat +datum=WGS84")
 
+coords <- centroid %>%
+  st_coordinates() %>%
+  as.data.frame() %>%
+  rename(long = X, lat = Y)
+
+centroid <- cbind(centroid, coords)
+
+#
+# Circle Polygons ---------------------------------------
+#
+
+spCircle(radius,
+         spUnits = CRS(projargs = as.character(NA)),
+         centerPoint = c(x = 0, y = 0),
+         nptsPerimeter = 100,
+         spID = paste("circle", .StemEnv$randomID(), sep = ":"),
+         ...)
+
+#
+# TravelTime Polygon Maps --------------------------------
+#
 
 readRenviron("~/.Renviron")
 traveltime_api <- Sys.getenv("TRAVELAPI")
 traveltime_id <- Sys.getenv("TRAVELID")
 
-colors <- c("#232d4b","#2c4f6b","#0e879c","#60999a","#d1e0bf","#d9e12b","#e6ce3a","#e6a01d","#e57200","#fdfdfd")
-
-for(i in 1:nrow(emsstations)){
-  i=9
-  ems_iso8 <- traveltime_map(appId= traveltime_id,
+for(i in 1:nrow(centroid)){
+  park_iso5 <- traveltime_map(appId= traveltime_id,
                              apiKey = traveltime_api,
-                             location=c(emsstations$LATITUDE[i],emsstations$LONGITUDE[i]),
-                             traveltime=480,
-                             type="driving",
-                             departure="2020-08-07T08:00:00+01:00")
-  saveRDS(ems_iso8, file = paste0('ems_iso_8_',i,'.RDS'))
-  ems_iso10 <- traveltime_map(appId= traveltime_id,
+                             location=c(centroid$lat[i],centroid$long[i]),
+                             traveltime=300,
+                             type="walking",
+                             departure="2021-08-07T08:00:00+01:00")
+  
+  park_iso10 <- traveltime_map(appId= traveltime_id,
                               apiKey = traveltime_api,
-                              location=c(emsstations$LATITUDE[i],emsstations$LONGITUDE[i]),
+                              location=c(centroid$lat[i],centroid$long[i]),
                               traveltime=600,
-                              type="driving",
-                              departure="2020-08-07T08:00:00+01:00")
-  saveRDS(ems_iso10, file = paste0('ems_iso_10_',i,'.RDS'))
-  ems_iso12 <- traveltime_map(appId= traveltime_id,
+                              type="walking",
+                              departure="2021-08-07T08:00:00+01:00")
+  
+  park_iso15 <- traveltime_map(appId= traveltime_id,
                               apiKey = traveltime_api,
-                              location=c(emsstations$LATITUDE[i],emsstations$LONGITUDE[i]),
-                              traveltime=720,
-                              type="driving",
-                              departure="2020-08-07T08:00:00+01:00")
-  saveRDS(ems_iso12, file = paste0('ems_iso_12_',i,'.RDS'))
-  residential = mapview(st_geometry(residential_sf), cex =.5, layer.name = "residential areas", color = colors[5])
-  m1 = mapview(ems_iso8, layer.name = "8 minute isochrone", col.regions = colors[1])
-  m2 = mapview(ems_iso10, layer.name = "10 minute isochrone", col.regions = colors[2])
-  m3 = mapview(ems_iso12, layer.name = "12 minute isochrone", col.regions = colors[3])
-  # add the second layer on top
-  m1 = m1 + m2 + m3 + residential
-  mapshot(m1, file = paste0("~/git/dspg2020patrick/output/isochrone_maps/emsmap_",i, ".png", sep = ""))
+                              location=c(centroid$lat[i],centroid$long[i]),
+                              traveltime=900,
+                              type="walking",
+                              departure="2021-08-07T08:00:00+01:00")
+  
+  saveRDS(park_iso5, file = paste0('./data/working/traveltime_isochrones/park_iso_5_',i,'.RDS'))
+  saveRDS(park_iso10, file = paste0('./data/working/traveltime_isochrones/park_iso_10_',i,'.RDS'))
+  saveRDS(park_iso15, file = paste0('./data/working/traveltime_isochrones/park_iso_15_',i,'.RDS'))
+  
+  m1 = mapview(park_iso5, layer.name = "5 minute isochrone", col.regions = colors[1])
+  m2 = mapview(park_iso10, layer.name = "10 minute isochrone", col.regions = colors[2])
+  m3 = mapview(park_iso15, layer.name = "15 minute isochrone", col.regions = colors[3])
+  
+  m1 = m1 + m2 + m3
+  
+  #this isn't working and I'm mad
+  mapshot(m1, file = paste0("./output/leaflet/park_tt_map_",i, ".png", sep = ""))
 }
+
+
