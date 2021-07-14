3daea3e12faa2952bf00635449201087ca6bcdb5
Morgan Stockham <morganstockham@gmail.com>
2021-07-13 21:04:12 -0400

    fixed intersection issue, did some simple t tests

--- src/09_subcnty_intersections.R
+++ src/09_subcnty_intersections.R
@@ -53,24 +53,58 @@ write_rds(parks, "./data/working/parks.Rds")
 # Coverage at the Block and Group Level --------------------
 #
 
+length(unique(parks$tract_geoid))
+
 # they're still not merging :/
-area_bgrp <- left_join(acs_bgrp, parks, by = c("GEOID" = "geoid"))
-area_tract <- left_join(acs_tract, parks, by = c("GEOID" = "geoid"))
+area_bgrp <- left_join(acs_bgrp, parks, by = c("GEOID" = "bgrp_geoid"))
+area_tract <- left_join(acs_tract, parks, by = c("GEOID" = "tract_geoid"))
 
 area_bgrp <- area_bgrp %>% 
+  as.data.frame() %>%
   select(GEOID, Acreage) %>%
+  na.omit() %>%
   group_by(GEOID) %>%
   mutate(acres = mean(Acreage, na.rm = T), count = n()) %>%
+  unique() %>%
+  select(GEOID, acres, count) %>%
   unique()
 
 area_tract <- area_tract %>% 
+  as.data.frame() %>%
   select(GEOID, Acreage) %>%
   group_by(GEOID) %>%
   mutate(acres = mean(Acreage, na.rm = T), count = n()) %>%
+  unique() %>%
+  select(GEOID, acres, count) %>%
   unique()
 
 acs_bgrp <- left_join(acs_bgrp, area_bgrp, by = c("GEOID" = "GEOID"))
 acs_tract <- left_join(acs_tract, area_tract, by = c("GEOID" = "GEOID"))
 
-read_rds(acs_tract, "./data/working/acs_tract.Rds")
-read_rds(acs_bgrp, "./data/working/acs_bgrp.Rds")
+acs_bgrp$acres[is.nan(acs_bgrp$acres)] <- 0
+acs_tract$acres[is.nan(acs_tract$acres)] <- 0
+
+write_rds(acs_tract, "./data/working/acs_tract.Rds")
+write_rds(acs_bgrp, "./data/working/acs_bgrp.Rds")
+
+#
+# Test Differences in Groups -------------------
+#
+
+# differences between above median and below median black communities - less parks in black communities
+median(acs_tract$black, na.rm = T)
+acs_tract$black_group <- ifelse(acs_tract$black >= 6.2277, 1, 0)
+acs_tract$black_group[is.na(acs_tract$black_group)] <- 0
+var.test(acres ~ black_group, data = acs_tract)
+
+# differences between above median and below median hispanic communities - less parks in hispanic communities
+mean(acs_tract$hispanic, na.rm = T)
+acs_tract$hispanic_group <- ifelse(acs_tract$hispanic >= 14.52765, 1, 0)
+acs_tract$hispanic_group[is.na(acs_tract$hispanic_group)] <- 0
+var.test(acres ~ hispanic_group, data = acs_tract)
+
+# differences between above median and below median inpov communities - less parks in poor communities
+mean(acs_tract$inpov, na.rm = T)
+acs_tract$inpov_group <- ifelse(acs_tract$inpov >= 6.487571, 1, 0)
+acs_tract$inpov_group[is.na(acs_tract$inpov_group)] <- 0
+var.test(acres ~ inpov_group, data = acs_tract)
