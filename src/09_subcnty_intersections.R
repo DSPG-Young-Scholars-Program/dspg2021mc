361197ba5ade5efc4759ec471bd79ea66fd60fff
Eric Oh <ericoh17@gmail.com>
2021-07-21 16:21:00 -0400

    add utils script with geo2fips fcn

--- src/09_subcnty_intersections.R
+++ src/09_subcnty_intersections.R
@@ -3,6 +3,8 @@ library(tidygeocoder)
 library(sf)
 library(jsonlite)
 
+source("utils.R")
+
 acs_tract <- read_rds("./data/working/acs_tract.Rds")
 acs_bgrp <- read_rds("./data/working/acs_bgrp.Rds")
 
@@ -24,22 +26,6 @@ parks$state <- "Virginia"
 # use tinygeocoder to get lats and longs
 parks <- parks %>%
   geocode(street = address, county = county, state = state, method = "osm")
-
-# use eric's handy dandy function to get the geoid
-geo2fips <- function(latitude, longitude) {
-  url <- "https://geo.fcc.gov/api/census/area?lat=%f&lon=%f&format=json"
-  block <- jsonlite::fromJSON(sprintf(url, latitude, longitude))[["results"]][["block_fips"]]
-  
-  # get block group
-  block_grp <- substr(block, 1, 12)
-  block_grp <- ifelse(length(unique(block_grp)) == 1, unique(block_grp), block_grp[1])
-  
-  # get tract
-  tract <- substr(block, 1, 11)
-  tract <- ifelse(length(unique(tract)) == 1, unique(tract), tract[1])
-  
-  c(block_grp, tract)
-}
 
 parks_geoids <- apply(parks, 1, function(x) geo2fips(x$lat, x$long))
 
